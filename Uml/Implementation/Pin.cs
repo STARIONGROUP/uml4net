1697740427613df32b91e572eecb0f3055e0197d
samatrhea <s.gerene@rheagroup.com>
2019-02-26 15:58:45 +0100

    [Formatrting] cleanup of source files

--- Uml/Implementation/Pin.cs
+++ Uml/Implementation/Pin.cs
@@ -19,7 +19,6 @@
 // </copyright>
 // -------------------------------------------------------------------------------------------------
 
-
 namespace Implementation.Actions
 {
     using Uml.CommonStructure;
