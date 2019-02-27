09db5c885e6cf223c285f14b190ab3fa2a08ef46
samatrhea <s.gerene@rheagroup.com>
2019-02-27 08:16:57 +0100

    [Add] generalization to ControlFlow class

--- Uml/Activities/ControlFlow.cs
+++ Uml/Activities/ControlFlow.cs
@@ -24,7 +24,7 @@ namespace Uml.Activities
     /// <summary>
     /// A <see cref="ControlFlow"/> is an <see cref="ActivityEdge"/> traversed by control tokens or object tokens of control type, which are use to control the execution of <see cref="ExecutableNode"/>s.
     /// </summary>
-    public interface ControlFlow
+    public interface ControlFlow : ActivityEdge
     {
     }
 }
