c122790aac55f686d02c55f132e66a2c3cb1fc0e
samatrhea <s.gerene@rheagroup.com>
2019-02-27 08:47:59 +0100

    [Add] generalization to MergeNode class

--- Uml/Activities/MergeNode.cs
+++ Uml/Activities/MergeNode.cs
@@ -24,7 +24,7 @@ namespace Uml.Activities
     /// <summary>
     /// A merge node is a control node that brings together multiple alternate flows. It is not used to synchronize concurrent flows but to accept one among several alternate flows.
     /// </summary>
-    public interface MergeNode
+    public interface MergeNode : ControlNode
     {
     }
 }
