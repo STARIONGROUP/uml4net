2e8bbe66ed3b6eb70ffa5810a8cc8a331a724ba7
samatrhea <s.gerene@rheagroup.com>
2019-02-27 08:06:36 +0100

    [Add] properties and generalization to  ActivityNode  class

--- Uml/Activities/ActivityNode.cs
+++ Uml/Activities/ActivityNode.cs
@@ -19,12 +19,59 @@
 // </copyright>
 // -------------------------------------------------------------------------------------------------
 
+using Uml.Actions;
+
 namespace Uml.Activities
 {
+    using System.Collections.Generic;
+    using Uml.Classification;
+
     /// <summary>
     /// <see cref="ActivityNode"/> is an abstract class for points in the flow of an <see cref="Activity"/> connected by <see cref="ActivityEdge"/>s.
     /// </summary>
-    public interface ActivityNode
+    public interface ActivityNode : RedefinableElement
     {
+        /// <summary>
+        /// The <see cref="Activity"/> containing the <see cref="ActivityNode"/>, if it is directly owned by an <see cref="Activity"/>.
+        /// </summary>
+        Activity Activity { get; set; }
+
+        /// <summary>
+        /// <see cref="ActivityGroup"/>s containing the <see cref="ActivityNode"/>.
+        /// </summary>
+        /// <remarks>
+        /// Redefined property.
+        /// </remarks>
+        IEnumerable<ActivityGroup> InGroup { get; }
+
+        /// <summary>
+        /// <see cref="InterruptibleActivityRegion"/>s containing the <see cref="ActivityNode"/>.
+        /// </summary>
+        List<InterruptibleActivityRegion> InInterruptibleRegion { get; set; }
+
+        /// <summary>
+        /// <see cref="ActivityPartition"/>s containing the <see cref="ActivityNode"/>.
+        /// </summary>
+        List<ActivityPartition> InPartition { get; set; }
+
+        /// <summary>
+        /// The <see cref="StructuredActivityNode"/> containing the <see cref="ActvityNode"/>, if it is directly owned by a <see cref="StructuredActivityNode"/>.
+        /// </summary>
+        StructuredActivityNode InStructuredNode { get; set; }
+
+        /// <summary>
+        /// <see cref="ActivityEdge"/>s that have the <see cref="ActivityNode"/> as their target.
+        /// </summary>
+        List<ActivityEdge> Incoming { get; set; }
+
+        /// <summary>
+        /// <see cref="ActivityEdge"/>s that have the <see cref="ActivityNode"/> as their source.
+        /// </summary>
+        List<ActivityEdge> Outgoing { get; set; }
+
+        /// <summary>
+        /// <see cref="ActivityNode"/>s from a generalization of the <see cref="Activity"/> containing this <see cref="ActivityNode"/> that are redefined by this <see cref="ActivityNode"/>.
+        /// </summary>
+        List<ActivityNode> RedefinedNode { get; set; }
     }
 }
