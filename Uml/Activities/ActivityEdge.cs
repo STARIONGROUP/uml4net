01e29fa7f307e7f6c66fd19eceef3f27cf1528dd
samatrhea <s.gerene@rheagroup.com>
2019-02-27 07:18:26 +0100

    [Add] properties and generalization to  ActivityEdge class

--- Uml/Activities/ActivityEdge.cs
+++ Uml/Activities/ActivityEdge.cs
@@ -21,10 +21,67 @@
 
 namespace Uml.Activities
 {
+    using System.Collections.Generic;
+    using Uml.Actions;
+    using Uml.Classification;
+    using Uml.Values;
+
     /// <summary>
     /// An ActivityEdge is an abstract class for directed connections between two ActivityNodes.
     /// </summary>
-    public interface ActivityEdge
+    public interface ActivityEdge : RedefinableElement
     {
+        /// <summary>
+        /// The <see cref="Activity"/> containing the <see cref="ActivityEdge"/>, if it is directly owned by an <see cref="Activity"/>.
+        /// </summary>
+        Activity Activity { get; set; } 
+
+        /// <summary>
+        /// A <see cref="ValueSpecification"/> that is evaluated to determine if a token can traverse the <see cref="ActivityEdge"/>. If an <see cref="ActivityEdge"/> has no guard, then there is no restriction on tokens traversing the edge.
+        /// </summary>
+        ValueSpecification Guard { get; set; }
+
+        /// <summary>
+        /// <see cref="ActivityGroup"/>s containing the <see cref="ActivityEdge"/>.
+        /// </summary>
+        /// <remarks>
+        /// derived property
+        /// </remarks>
+        IEnumerable<ActivityGroup>  InGroup { get; }
+
+        /// <summary>
+        /// <see cref="ActivityPartition"/>s containing the <see cref="ActivityEdge"/>.
+        /// </summary>
+        List<ActivityPartition> InPartition { get; set; }
+
+        /// <summary>
+        /// The <see cref="StructuredActivityNode"/> containing the <see cref="ActivityEdge"/>, if it is owned by a <see cref="StructuredActivityNode"/>.
+        /// </summary>
+        StructuredActivityNode InStructuredNode { get; set; }
+
+        /// <summary>
+        /// The <see cref="InterruptibleActivityRegion"/> for which this <see cref="ActivityEdge"/> is an interruptingEdge.
+        /// </summary>
+        InterruptibleActivityRegion Interrupts { get; set; }
+
+        /// <summary>
+        /// <see cref="ActivityEdge"/>s from a generalization of the <see cref="Activity"/> containing this <see cref="ActivityEdge"/> that are redefined by this <see cref="ActivityEdge"/>.
+        /// </summary>
+        ActivityEdge RedefinedEdge { get; set; }
+
+        /// <summary>
+        /// The ActivityNode from which tokens are taken when they traverse the ActivityEdge.
+        /// </summary>
+        ActivityNode Source { get; set; }
+
+        /// <summary>
+        /// The <see cref="ActivityNode"/> to which tokens are put when they traverse the <see cref="ActivityEdge"/>.
+        /// </summary>
+        ActivityNode Target { get; set; }
+
+        /// <summary>
+        /// The minimum number of tokens that must traverse the <see cref="ActivityEdge"/> at the same time. If no weight is specified, this is equivalent to specifying a constant value of 1.
+        /// </summary>
+        ValueSpecification Weight { get; set; }
     }
 }
