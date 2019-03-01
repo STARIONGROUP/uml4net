7517a5c7423d5aa84c7f5c16a96a591a8048ebfd
samatrhea <s.gerene@rheagroup.com>
2019-03-01 08:57:56 +0100

    [Add] properties and generalization to classes in InformationFlows namespace

--- Uml/InformationFlows/InformationFlow.cs
+++ Uml/InformationFlows/InformationFlow.cs
@@ -15,16 +15,57 @@
 //   GNU General Public License for more details.
 //
 //   You should have received a copy of the GNU General Public License
-//   along with Foobar.  If not, see<http://www.gnu.org/licenses/>.
+//   along with uml-sharp. If not, see<http://www.gnu.org/licenses/>.
 // </copyright>
 // -------------------------------------------------------------------------------------------------
 
 namespace Uml.InformationFlows
 {
+    using System.Collections.Generic;
+    using Uml.Activities;
+    using Uml.Classification;
+    using Uml.CommonStructure;
+    using Uml.Interactions;
+    using Uml.StructuredClassifiers;
+
     /// <summary>
     /// InformationFlows describe circulation of information through a system in a general manner. They do not specify the nature of the information, mechanisms by which it is conveyed, sequences of exchange or any control conditions. During more detailed modeling, representation and realization links may be added to specify which model elements implement an InformationFlow and to show how information is conveyed.  InformationFlows require some kind of “information channel” for unidirectional transmission of information items from sources to targets.  They specify the information channel’s realizations, if any, and identify the information that flows along them.  Information moving along the information channel may be represented by abstract InformationItems and by concrete Classifiers.
     /// </summary>
-    public interface InformationFlow
+    public interface InformationFlow : DirectedRelationship, PackageableElement
     {
+        /// <summary>
+        /// Specifies the information items that may circulate on this information flow.
+        /// </summary>
+        List<Classifier> Conveyed { get; set; }
+
+        /// <summary>
+        /// Defines from which source the conveyed <see cref="InformationItem"/>s are initiated.
+        /// </summary>
+        List<NamedElement> InformationSource { get; set; }
+
+        /// <summary>
+        /// Defines to which target the conveyed <see cref="InformationItem"/>s are directed.
+        /// </summary>
+        List<NamedElement> InformationTarget { get; set; }
+
+        /// <summary>
+        /// Determines which <see cref="Relationship"/> will realize the specified flow.
+        /// </summary>
+        List<Relationship> Realization { get; set; }
+
+        /// <summary>
+        /// Determines which <see cref="ActivityEdge"/>s will realize the specified flow.
+        /// </summary>
+        List<ActivityEdge> RealizingActivityEdge { get; set; }
+
+        /// <summary>
+        /// Determines which <see cref="Connector"/>s will realize the specified flow.
+        /// </summary>
+        List<Connector> RealizingConnector { get; set; }
+
+        /// <summary>
+        /// Determines which <see cref="Message"/>s will realize the specified flow.
+        /// </summary>
+        List<Message> RealizingMessage { get; set; }
     }
 }
--- Uml/InformationFlows/InformationItem.cs
+++ Uml/InformationFlows/InformationItem.cs
@@ -15,16 +15,23 @@
 //   GNU General Public License for more details.
 //
 //   You should have received a copy of the GNU General Public License
-//   along with Foobar.  If not, see<http://www.gnu.org/licenses/>.
+//   along with uml-sharp. If not, see<http://www.gnu.org/licenses/>.
 // </copyright>
 // -------------------------------------------------------------------------------------------------
 
 namespace Uml.InformationFlows
 {
+    using System.Collections.Generic;
+    using Uml.Classification;
+
     /// <summary>
     /// InformationItems represent many kinds of information that can flow from sources to targets in very abstract ways.  They represent the kinds of information that may move within a system, but do not elaborate details of the transferred information.  Details of transferred information are the province of other Classifiers that may ultimately define InformationItems.  Consequently, InformationItems cannot be instantiated and do not themselves have features, generalizations, or associations. An important use of InformationItems is to represent information during early design stages, possibly before the detailed modeling decisions that will ultimately define them have been made. Another purpose of InformationItems is to abstract portions of complex models in less precise, but perhaps more general and communicable, ways. 
     /// </summary>
-    public interface InformationItem
+    public interface InformationItem : Classifier
     {
+        /// <summary>
+        /// Determines the classifiers that will specify the structure and nature of the information. An information item represents all its represented classifiers.
+        /// </summary>
+        List<Classifier> Represented { get; set; }
     }
 }
