4550238f4d9e2a627084fe009520ff00298f9759
samatrhea <s.gerene@rheagroup.com>
2019-03-03 09:29:12 +0100

    [Fix] copyright header

--- Uml/Actions/CallAction.cs
+++ Uml/Actions/CallAction.cs
@@ -15,7 +15,7 @@
 //   GNU General Public License for more details.
 //
 //   You should have received a copy of the GNU General Public License
-//   along with uml-sharp.  If not, see<http://www.gnu.org/licenses/>.
+//   along with uml-sharp. If not, see<http://www.gnu.org/licenses/>.
 // </copyright>
 // -------------------------------------------------------------------------------------------------
 
