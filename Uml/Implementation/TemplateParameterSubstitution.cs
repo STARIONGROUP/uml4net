cd869e349c2d3f95abd4a88c2026d284026a6150
samatrhea <s.gerene@rheagroup.com>
2019-03-03 12:22:30 +0100

    [Implement] TemplateParameterSubstitution

--- Uml/Implementation/TemplateParameterSubstitution.cs
+++ Uml/Implementation/TemplateParameterSubstitution.cs
@@ -21,12 +21,43 @@
 
 namespace Implementation.CommonStructure
 {
+    using Uml.Assembler;
     using Uml.CommonStructure;
 
     /// <summary>
     /// A <see cref="TemplateParameterSubstitution"/> relates the actual parameter to a formal <see cref="TemplateParameter"/> as part of a template binding.
     /// </summary>
-    internal class TemplateParameterSubstitution : Element, Uml.CommonStructure.TemplateParameterSubstitution
+    internal class TemplateParameterSubstitution : Implementation.CommonStructure.Element, Uml.CommonStructure.TemplateParameterSubstitution
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemplateParameterSubstitution"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// The unique identifier of the <see cref="TemplateParameterSubstitution"/>
+        /// </param>
+        public TemplateParameterSubstitution(string id) : base(id)
+        {
+            this.OwnedActual = new OwnerList<ParameterableElement>(this);
+        }
+
+        /// <summary>
+        /// The <see cref="ParameterableElement"/> that is the actual parameter for this <see cref="TemplateParameterSubstitution"/>.
+        /// </summary>
+        public ParameterableElement Actual { get; set; }
+
+        /// <summary>
+        /// The formal <see cref="TemplateParameter"/> that is associated with this <see cref="TemplateParameterSubstitution"/>.
+        /// </summary>
+        public Uml.CommonStructure.TemplateParameter Formal { get; set; }
+
+        /// <summary>
+        /// The <see cref="ParameterableElement"/> that is owned by this <see cref="TemplateParameterSubstitution"/> as its actual parameter.
+        /// </summary>
+        public OwnerList<ParameterableElement> OwnedActual { get; set; }
+
+        /// <summary>
+        /// The <see cref="TemplateBinding"/> that owns this <see cref="TemplateParameterSubstitution"/>.
+        /// </summary>
+        public Uml.CommonStructure.TemplateBinding TemplateBinding { get; set; }
     }
 }
