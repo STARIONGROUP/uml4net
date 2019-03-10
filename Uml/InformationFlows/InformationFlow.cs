49a869d6508e140d9310987baa57a3548ee4cbb3
samatrhea <s.gerene@rheagroup.com>
2019-03-10 08:46:20 +0100

    [Add] Class and Property attributes to Interfaces in the InformationFlows namespace; fixes #20

--- Uml/InformationFlows/InformationFlow.cs
+++ Uml/InformationFlows/InformationFlow.cs
@@ -23,49 +23,65 @@ namespace Uml.InformationFlows
 {
     using System.Collections.Generic;
     using Uml.Activities;
+    using Uml.Attributes;
     using Uml.Classification;
     using Uml.CommonStructure;
     using Uml.Interactions;
     using Uml.StructuredClassifiers;
 
     /// <summary>
-    /// InformationFlows describe circulation of information through a system in a general manner. They do not specify the nature of the information, mechanisms by which it is conveyed, sequences of exchange or any control conditions. During more detailed modeling, representation and realization links may be added to specify which model elements implement an InformationFlow and to show how information is conveyed.  InformationFlows require some kind of “information channel” for unidirectional transmission of information items from sources to targets.  They specify the information channel’s realizations, if any, and identify the information that flows along them.  Information moving along the information channel may be represented by abstract InformationItems and by concrete Classifiers.
+    /// <see cref="InformationFlow"/>s describe circulation of information through a system in a general manner. They do not specify the nature of the information, mechanisms by which it is conveyed, sequences of exchange or any control conditions. During more detailed modeling, representation and realization links may be added to specify which model elements implement an <see cref="InformationFlow"/> and to show how information is conveyed.  InformationFlows require some kind of “information channel” for unidirectional transmission of information items from sources to targets.  They specify the information channel’s realizations, if any, and identify the information that flows along them.  Information moving along the information channel may be represented by abstract <see cref="InformationItem"/>s and by concrete <see cref="Classifier"/>s.
     /// </summary>
+    [Class(IsAbstract = false, IsActive = false, Specializations = "")]
     public interface InformationFlow : DirectedRelationship, PackageableElement
     {
         /// <summary>
         /// Specifies the information items that may circulate on this information flow.
         /// </summary>
+        [MultiplicityElement(IsOrdered = false, IsUnique = true, Lower = 1, Upper = "*")]
+        [Property(IsDerived = false, IsDerivedUnion = false, IsReadOnly = false, IsStatic = false, Aggregation = AggregationKind.None, SubsettedProperty = "", RedefinedProperty = "")]
         List<Classifier> Conveyed { get; set; }
 
         /// <summary>
         /// Defines from which source the conveyed <see cref="InformationItem"/>s are initiated.
         /// </summary>
+        [MultiplicityElement(IsOrdered = false, IsUnique = true, Lower = 1, Upper = "*")]
+        [Property(IsDerived = false, IsDerivedUnion = false, IsReadOnly = false, IsStatic = false, Aggregation = AggregationKind.None, SubsettedProperty = "DirectedRelationship.Source", RedefinedProperty = "")]
         List<NamedElement> InformationSource { get; set; }
 
         /// <summary>
         /// Defines to which target the conveyed <see cref="InformationItem"/>s are directed.
         /// </summary>
+        [MultiplicityElement(IsOrdered = false, IsUnique = true, Lower = 1, Upper = "*")]
+        [Property(IsDerived = false, IsDerivedUnion = false, IsReadOnly = false, IsStatic = false, Aggregation = AggregationKind.None, SubsettedProperty = "DirectedRelationship.Target", RedefinedProperty = "")]
         List<NamedElement> InformationTarget { get; set; }
 
         /// <summary>
         /// Determines which <see cref="Relationship"/> will realize the specified flow.
         /// </summary>
+        [MultiplicityElement(IsOrdered = false, IsUnique = true, Lower = 0, Upper = "*")]
+        [Property(IsDerived = false, IsDerivedUnion = false, IsReadOnly = false, IsStatic = false, Aggregation = AggregationKind.None, SubsettedProperty = "", RedefinedProperty = "")]
         List<Relationship> Realization { get; set; }
 
         /// <summary>
         /// Determines which <see cref="ActivityEdge"/>s will realize the specified flow.
         /// </summary>
+        [MultiplicityElement(IsOrdered = false, IsUnique = true, Lower = 0, Upper = "*")]
+        [Property(IsDerived = false, IsDerivedUnion = false, IsReadOnly = false, IsStatic = false, Aggregation = AggregationKind.None, SubsettedProperty = "", RedefinedProperty = "")]
         List<ActivityEdge> RealizingActivityEdge { get; set; }
 
         /// <summary>
         /// Determines which <see cref="Connector"/>s will realize the specified flow.
         /// </summary>
+        [MultiplicityElement(IsOrdered = false, IsUnique = true, Lower = 0, Upper = "*")]
+        [Property(IsDerived = false, IsDerivedUnion = false, IsReadOnly = false, IsStatic = false, Aggregation = AggregationKind.None, SubsettedProperty = "", RedefinedProperty = "")]
         List<Connector> RealizingConnector { get; set; }
 
         /// <summary>
         /// Determines which <see cref="Message"/>s will realize the specified flow.
         /// </summary>
+        [MultiplicityElement(IsOrdered = false, IsUnique = true, Lower = 0, Upper = "*")]
+        [Property(IsDerived = false, IsDerivedUnion = false, IsReadOnly = false, IsStatic = false, Aggregation = AggregationKind.None, SubsettedProperty = "", RedefinedProperty = "")]
         List<Message> RealizingMessage { get; set; }
     }
 }
