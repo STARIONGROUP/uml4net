4ddf489d5e68fd52be63bc543da340187bd4bb59
samatstarion <s.gerene@stariongroup.eu>
2024-11-16 12:35:12 +0100

    [Add] uml4net.Reporting.Tests

---
+++ uml4net.Reporting.Tests.csproj/ClassExtensionsTestFixture.cs
@@ -0,0 +1,87 @@
+// -------------------------------------------------------------------------------------------------
+//  <copyright file="ClassExtensionsTestFixture.cs" company="Starion Group S.A.">
+// 
+//    Copyright 2019-2024 Starion Group S.A.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, softwareUseCases
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+namespace uml4net.Extensions.Tests
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.Extensions.Logging;
+
+    using NUnit.Framework;
+
+    using Serilog;
+
+    using uml4net.POCO.Packages;
+    using uml4net.POCO.StructuredClassifiers;
+    using uml4net.xmi;
+
+    [TestFixture]
+    public class ClassExtensionsTestFixture
+    {
+        private ILoggerFactory loggerFactory;
+
+        private IEnumerable<IPackage> packages;
+
+        private IPackage rootPackage;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            this.loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddSerilog();
+            });
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var rootPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData");
+
+            var reader = XmiReaderBuilder.Create()
+                .UsingSettings(x => x.LocalReferenceBasePath = rootPath)
+                .WithLogger(this.loggerFactory)
+                .Build();
+
+            this.packages = reader.Read(Path.Combine(rootPath, "UML.xmi"));
+
+            this.rootPackage = this.packages.Single();
+        }
+
+        [Test]
+        public void Verify_that_QueryAllProperties_returns_expected_result()
+        {
+            var commonStructuresPackage = this.rootPackage.NestedPackage.Single(x => x.Name == "CommonStructure");
+
+            var dependency = commonStructuresPackage.PackagedElement.OfType<IClass>().Single(x => x.Name == "Dependency");
+
+            var properties = dependency.QueryAllProperties();
+
+            Assert.That(properties.Count, Is.EqualTo(17));
+        }
+    }
+}
