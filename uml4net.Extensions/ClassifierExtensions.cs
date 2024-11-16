630cfa3035735a0b5de27301e06e27b3938d7eab
samatstarion <s.gerene@stariongroup.eu>
2024-11-16 12:30:55 +0100

    [Fix] copyright header

--- uml4net.Extensions/ClassifierExtensions.cs
+++ uml4net.Extensions/ClassifierExtensions.cs
@@ -1,15 +1,22 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ClassifierExtensions.cs" company="Starion Group N.V.">
-//    Copyright (c) 2024 Starion Group N.V.
+﻿// -------------------------------------------------------------------------------------------------
+//  <copyright file="ClassifierExtensions.cs" company="Starion Group S.A.">
 // 
-//    Author: Seliman Niakate, Sebastien d'Antuono, Nathanael Smiechowski, Amine Nemmaoui, Amir Janati.
+//    Copyright 2019-2024 Starion Group S.A.
 // 
-//    This file is part of the SafePlace solution.
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
 // 
-//    SafePlace is distributed under the terms and conditions of the
-//    European Space Agency Public License (ESA-PL) Weak Copyleft (Type 2) – v2.4
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, softwareUseCases
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
 
 namespace uml4net.Extensions
 {
