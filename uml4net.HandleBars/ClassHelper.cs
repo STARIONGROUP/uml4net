72f832a0eee999464b895c2467e478736676862a
atheate <a.theate@stariongroup.eu>
2026-03-26 11:47:31 +0100

    Html report provide operation information

--- uml4net.HandleBars/ClassHelper.cs
+++ uml4net.HandleBars/ClassHelper.cs
@@ -24,6 +24,7 @@ namespace uml4net.HandleBars
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Xml.Linq;
 
     using HandlebarsDotNet;
 
@@ -286,6 +287,21 @@ namespace uml4net.HandleBars
                 {
                     options.Template(output, svg);
                 }
+            });
+            
+            handlebars.RegisterHelper("Class.QueryAllOperations", (_, arguments) =>
+            {
+                if (arguments.Length != 1)
+                {
+                    throw new ArgumentException("#Class.QueryAllOperations expects to have  exactly 1 argument");
+                }
+
+                if (arguments[0] is not IClass umlClass)
+                {
+                    throw new ArgumentException("#Class.QueryAllOperations argument should be an IClass");
+                }
+
+                return umlClass.QueryAllOperations();
             });
         }
     }
